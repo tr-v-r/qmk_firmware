cba4449deeedb7199c80bfcc0ad9a939a5256d03
tr-v-r <83373608+tr-v-r@users.noreply.github.com>
2022-01-16 23:40:34 -0500

    Updated personal keyboard map to resolve issue with right side being reversed.

--- keyboards/ferris/keymaps/tr-v-r/config.h
+++ keyboards/ferris/keymaps/tr-v-r/config.h
@@ -19,5 +19,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 200
 #define PERMISSIVE_HOLD
-#define IGNORE_MOD_TAP_INTERRUPT //keep?
+#define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
